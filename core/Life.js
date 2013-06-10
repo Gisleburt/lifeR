ae97b23f52b4cf3a4de686f1a3447eb9c109d60b
Daniel Mason <daniel@loftdigital.com>
2013-06-10 15:44:32 +0100

    Moved to separate file, started making it 'modular'

---
+++ core/Life.js
@@ -0,0 +1,63 @@
+var Life = function(playerElement) {
+	this.player = playerElement;
+	this.addLifeContainer(this.player);
+	this.life = 20;
+}
+
+
+// Add Life Container
+Life.prototype.addLifeContainer = function(player) {
+
+	var id = player.id+'life';
+	var element = document.getElementById(id);
+
+	if(typeof(element) == 'undefined' || element == null) {
+
+		var section;
+		var subtract, subtractText;
+		var add, addText;
+		var life;
+		var parentObj = this;
+
+		section = document.createElement('section');
+		section.id        = id;
+		section.className = 'lifeContainer';
+
+		subtractText       = document.createTextNode('-');
+		subtract           = document.createElement('a');
+		subtract.className = 'subtract';
+		subtract.href      = '#';
+		subtract.onclick   = function() { parentObj.life--;};
+		subtract.appendChild(subtractText);
+
+		addText       = document.createTextNode('+');
+		add           = document.createElement('a');
+		add.className = 'add';
+		add.href      = '#';
+		add.onclick   = function() { parentObj.life++;};
+		add.appendChild(addText);
+
+		life           = document.createElement('span');
+		life.className = 'life';
+
+		section.appendChild(subtract);
+		section.appendChild(add);
+		section.appendChild(life);
+
+		player.appendChild(section);
+	}
+
+}
+
+// Get and set life.
+Object.defineProperty(Life.prototype, "life", {
+	get: function() { return Number(this.player.getElementsByClassName('life')[0].innerHTML) },
+	set: function(value) { console.log(value); this.player.getElementsByClassName('life')[0].innerHTML = Number(value) }
+});
+
+for(var player in players) {
+
+	players[player].modules.life = new Life(players[player]);
+
+}
+
